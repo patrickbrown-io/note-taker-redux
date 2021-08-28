290d928b96cd5432306cb0b55c4120bf213e42ad
Patrick Brown <breepatbree@gmail.com>
2021-08-28 09:49:28 -0600

    Save working, still wont populate list

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-////GET////
+////////////////////////////GET/////////////////////////////////
 app.get('/', (req,res) =>
     res.sendFile(path.join(__dirname, './public/index.html'))
 );
@@ -20,6 +20,62 @@ app.get('/', (req,res) =>
 app.get('/notes', (req,res) =>
     res.sendFile(path.join(__dirname, './public/notes.html'))
 );
+///GET Route for /api/notes
+const data = require('./db/db.json');
+app.get('/api/notes', (req,res) =>
+    res.status(200).json(data));
+
+
+
+// POST request to add a review
+app.post('/api/notes', (req, res) => {
+  // Log that a POST request was received
+  console.log(req.body);
+
+  // Destructuring assignment for the items in req.body
+  const { title, text } = req.body;
+
+  // If all the required properties are present
+  if (title && text) {
+    // Variable for the object we will save
+    const newNote = {
+      title,
+      text,
+    };
+    // Obtain existing reviews
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+      } else {
+        // Convert string into JSON object
+        const parsedNotes = JSON.parse(data);
+
+        // Add a new review
+        parsedNotes.push(newNote);
+
+        // Write updated reviews back to the file
+        fs.writeFile(
+          './db/db.json',
+          JSON.stringify(parsedNotes, null, 4),
+          (writeErr) =>
+            writeErr
+              ? console.error(writeErr)
+              : console.info('Successfully updated notes!')
+        );
+      }
+    });
+
+    const response = {
+      status: 'success',
+      body: newNote,
+    };
+
+    console.log(response);
+    res.status(201).json(response).send();
+  } else {
+    res.status(500).json('Error in posting review');
+  }
+});
 
 //LISTENING
 app.listen(PORT, () =>
